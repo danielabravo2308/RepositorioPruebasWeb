1ae44d5aa6f2d70c7895a0aed315397a77809fbd
danibraru <danielabravorueda@gmail.com>
2025-09-04 22:31:33 -0400

    mejorando los test cases en el archivo title.multi.spec.js

---
+++ tests/title.multi.spec.js
@@ -0,0 +1,26 @@
+const {test,expect} = require('@playwright/test')
+
+const casos = [
+    {name:"Caso 1", url:"https://example.com/", esperado: "Example Domain"},
+    {name: "Caso 2" , url:"https://example.com/", esperado: /Example/},
+    {name:"Caso 3", url:"https://google.com/", esperado: "Google"},
+    {name:"Caso 4", url:"https://google.com/", esperado: /Google/},
+    {name:"Caso 5", url:"https://youtube.com/", esperado: "YouTube"},
+    {name:"Caso 6", url:"https://youtube.com/", esperado: /YouTube/}
+
+];
+
+for (const {name, url, esperado} of casos) {
+    test(`valida el titulo correcto en ${name}`,async ({page}) => {
+        // Abre la pagina example.com
+        await page.goto(url);
+        await sleep(3000);
+
+        //Verifiacion del titulo exacto
+        await expect(page).toHaveTitle(esperado);
+    });
+}
+
+function sleep (time){
+  return new Promise((resolve)=> setTimeout(resolve,time));
+}
--- tests/title.spec.js
+++ tests/title.spec.js
@@ -2,7 +2,9 @@ const {test , expect} = require('@playwright/test')
 
 test ('valida el titulo exacto', async({page})=> {
 //Abre pagina example.com
-await page.goto('https://example.com')
+await page.goto('https://example.com/')
+await sleep(5000);
+
 // Verificacion del titulo exacto
 await expect(page).toHaveTitle('Example Domain')
 
@@ -12,9 +14,13 @@ await expect(page).toHaveTitle('Example Domain')
 
 test ('valida titulo con regex', async ({page})=> {
   //Abre pagina example.com
-await page.goto('https://example.com')
-
+await page.goto('https://example.com/')
+await sleep(5000);
 // Verificacion palabra 
 await expect(page).toHaveTitle(/Example/)
 
 });
+
+function sleep (time){
+  return new Promise((resolve)=> setTimeout(resolve,time));
+}
